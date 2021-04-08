91eb0533433bacd5b3b06ff2c81fb828e43484cd
Greg <maza.gregory46@gmail.com>
2021-04-08 15:18:21 +0200

    Etape 5 - Démarrer un serveur web qui sait servir des fichiers statiques

---
+++ main.js
@@ -0,0 +1,6 @@
+const http = require('http'); // Fait appel à http.js
+const app = require('./server'); // 
+
+const server = http.createServer(app); // Créer le serveur
+app.set('port', process.env.PORT || 3000); // définir le port du serveur
+server.listen(process.env.PORT || 3000); // demander au serveur d'écouter sur le port 3000
