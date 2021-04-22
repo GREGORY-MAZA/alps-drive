30501f65687db9bc58a5c2aa7f0d3662744d9ba9
Greg <maza.gregory46@gmail.com>
2021-04-22 12:07:57 +0200

    Feat add function createFolder deleteFolder

--- drive.js
+++ drive.js
@@ -1,4 +1,5 @@
 const fs = require('fs/promises') // fs devient un objet
+
 function listFolder() {
     const readPromise = fs.readdir('.', { withFileTypes: true })
     const transformResultPromise = readPromise.then((dirents) => {
@@ -15,6 +16,35 @@ function listFolder() {
     return transformResultPromise;
 }
 
+function createFolder(name) {
+    const myCreateFolderPromise = fs.mkdir(name)
+    return myCreateFolderPromise
+}
+
+function deleteFolder(name) {
+    const myDeleteFolderPromise = fs.rmdir(name)
+    return myDeleteFolderPromise
+}
+
+function isFileOrFolder(name) {
+    const isFileOrFolderPromise = fs.stat(name) // Donne les infos
+    console.log(isFileOrFolderPromise)
+    return isFileOrFolderPromise
+}
+
+function displayContent(name) {
+    const read = fs.readFile(name, "utf8", function(err, data) {
+        if (err) {
+            return console.log(err)
+        }
+    })
+    return read
+}
+
 module.exports = { //création d'objet
-    functionListFolder: listFolder // le nom de la fonction : la fonction
+    functionListFolder: listFolder, // le nom de la fonction : la fonction
+    functionCreateFolder: createFolder,
+    functionDeleteFolder: deleteFolder,
+    functionisFileOrFolder: isFileOrFolder,
+    functionDisplayContent: displayContent
 }
--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 
 const express = require('express'); // importer express
-const drive = require('./drive'); // importer le module drive
+const drive = require('./drive'); // importer drive
 const app = express(); // créer notre application appeler avec la module express
 const port = 3000 // déclaration d'une variable port avec comme valeur 3000
 app.use(express.static('frontend')) // express.static est une fonction intégré dans express 
 
 app.get('/api/drive', (req, res) => {
-    const myFilesPromise = drive.functionListFolder() // appel de la fonction de cet objet (drive)
+    const myFilesPromise = drive.functionListFolder('/') // appel de la fonction de cet objet (drive)
     myFilesPromise.then((myFiles) => {
         console.log(myFiles)
         res.send(myFiles)
@@ -17,6 +17,40 @@ app.get('/api/drive', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.post('/api/drive', (req, res) => {
+    console.log("OK", req.query)
+    const myCreateFolder = drive.functionCreateFolder(req.query.name)
+    myCreateFolder.then((myFolder) => {
+        console.log(myFolder)
+        res.end(myFolder)
+    })
+
+})
+
+app.delete('/api/drive/:name', (req, res) => {
+    const myDeleteFolder = drive.functionDeleteFolder(req.params.name)
+    myDeleteFolder.then((myDelete) => {
+        res.send(myDelete)
+    })
+
+
+})
+
+app.get('/api/drive/:name', (req, res) => {
+    const name = req.params.name
+    const isFileorFolder = drive.functionisFileOrFolder(name)
+    isFileorFolder.then((isFile) => {
+        if (isFile) {
+            let file = drive.functionDisplayContent(name);
+            file.then((name) => {
+                res.send(name)
+
+            })
+        }
+
+    })
 })
 
 //module.exports = app; //exporter pour pouvoir y acceder avec notement notre serveur node
